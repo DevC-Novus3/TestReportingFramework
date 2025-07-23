5d314ebe5016045a498a18cbc45a6126e81021eb
DevonC <devonc@novus3.co.za>
2025-07-23 17:24:10 +0200

    🤌🏼

--- Backend/ReportingApp.Api/obj/Debug/net8.0/ReportingApp.Api.AssemblyInfo.cs
+++ Backend/ReportingApp.Api/obj/Debug/net8.0/ReportingApp.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportingApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60301bb5b980143a8de2ce54e4cbc4c6018e8b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportingApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportingApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
